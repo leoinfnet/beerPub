5bb06d5227ffdf7cfd54a75933aa15afff4897c7
leonardogloria <contato@leonardogloria.com.br>
2023-03-27 21:01:22 -0300

    Initial commit

---
+++ src/main/java/br/com/infnet/beerpub/BeerPubApplication.java
@@ -0,0 +1,13 @@
+package br.com.infnet.beerpub;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BeerPubApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(BeerPubApplication.class, args);
+    }
+
+}
